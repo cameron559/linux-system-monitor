61cbefd4e4f465d889b197beda249b1462c0e30a
cameron559 <cameronmilsom7@gmail.com>
2026-09-21 19:51:27 +0100

    Add basic system metric collection

---
+++ monitor.py
@@ -0,0 +1,20 @@
+import psutil
+
+def get_cpu_usage() -> float:
+    return psutil.cpu_percent(interval=1)
+
+def get_memory_usage() -> float:
+    return psutil.virtual_memory().percent
+
+def get_disk_usage() -> float:
+    return psutil.disk_usage("/").percent
+
+
+def get_system_metrics() -> dict:
+    return {
+        "cpu": get_cpu_usage(),
+        "memory": get_memory_usage(),
+        "disk": get_disk_usage()
+    }
+
+print(get_system_metrics())
