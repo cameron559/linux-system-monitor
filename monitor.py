93eea06c3d7b30213a57ad7d4ae4e71cc200ccb8
cameron559 <cameronmilsom7@gmail.com>
2026-09-22 18:26:13 +0100

    Add uptime and load average metrics

--- monitor.py
+++ monitor.py
@@ -10,16 +10,22 @@ def get_memory_usage() -> float:
 def get_disk_usage() -> float:
     return psutil.disk_usage("/").percent
 
+def get_uptime_seconds() -> float:
+    uptime = time.time() - psutil.boot_time()
+    return uptime
+
+def get_load_average() -> tuple[float, float, float]:
+    return psutil.getloadavg()
 
 def get_system_metrics() -> dict:
     return {
         "cpu": get_cpu_usage(),
         "memory": get_memory_usage(),
-        "disk": get_disk_usage()
+        "disk": get_disk_usage(),
+        "uptime": get_uptime_seconds(),
+        "load_average": get_load_average()
     }
 
-def get_uptime_seconds() -> float:
-    pass
+
 
 print(get_system_metrics())
-print(time)
